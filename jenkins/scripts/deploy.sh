d9f9f85cfad63fe95a1d315a4e2bd1336f95a98a
2102673 <90232284+2102673@users.noreply.github.com>
2023-11-03 18:28:53 +0800

    update
    update

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name myjenkins-blueocean -v C:\\Users\\there\\Documents\\GitHub\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name jenkins-php-selenium-test -v C:\\Users\\there\\Documents\\GitHub\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
 echo 'Now...'
-echo 'Visit http://192.168.0.13 to see your PHP application in action.'
+echo 'Visit http://localhost to see your PHP application in action.'
 
