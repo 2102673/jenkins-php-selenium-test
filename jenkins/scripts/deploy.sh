b6dcb2d7d8841ca20a606d325f5b5dcb800a3f7c
2102673 <90232284+2102673@users.noreply.github.com>
2023-11-04 12:59:55 +0800

    sdvfbg
    fdbgh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -3,7 +3,7 @@
 set -x
 pwd
 # Run the Docker container with a volume mount
-docker run -d -p 80:80 --name my-apache-php-app -mount C:\\Users\\there\\Documents\\GitHub\\jenkins-php-selenium-test\\src:/var/jenkins_home/workspace/jenkins-php-selenium-test/ php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -mount /var/jenkins_home/workspace/jenkins-php-selenium-test:/var/www/html/ php:7.2-apache
 sleep 1
 
 docker volume ls
