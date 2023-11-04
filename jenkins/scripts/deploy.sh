355584706a7e1b604d18b9c19a6345786cb57e1d
2102673 <90232284+2102673@users.noreply.github.com>
2023-11-04 12:50:38 +0800

    sdv
    sdv

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -6,6 +6,7 @@ set -x
 docker run -d -p 80:80 --name my-apache-php-app -v C:\\Users\\there\\Documents\\GitHub\\jenkins-php-selenium-test\\src:/var/jenkins_home/workspace/jenkins-php-selenium-test/ php:7.2-apache
 sleep 1
 
+docker volume ls
 pwd
 
 
