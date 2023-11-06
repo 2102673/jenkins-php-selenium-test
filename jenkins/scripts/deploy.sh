526bfd4f12c65b1e39ad60c38dc94ac42efe22dc
2102673 <90232284+2102673@users.noreply.github.com>
2023-11-06 19:06:04 +0800

    sdvc
    dsv

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
-
+pwd
 set -x
 # Run the Docker container with a volume mount
 docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace/jenkins-php-selenium-test/src:/var/www/html/ php:7.2-apache
 sleep 1
 set +x
-
+pwd
 
 echo 'Now...'
 echo 'Visit http://localhost to see your PHP application in action.'
