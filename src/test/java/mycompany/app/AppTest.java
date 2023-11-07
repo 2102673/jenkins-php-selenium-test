101d73d1033a026065ebd79fc7a289570c303cf6
2102673 <90232284+2102673@users.noreply.github.com>
2023-11-07 10:38:42 +0800

    zxc
    zxc

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "http://172.18.0.3";
+	String url = "http://172.18.16.83";
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
@@ -31,7 +31,7 @@ public class AppTest
 
     @Before
     public void setUp() { 
-		driver = new HtmlUnitDriver(); 
+		driver = new ChromeDriver(); 
 		wait = new WebDriverWait(driver, 10); 
 	} 
 
